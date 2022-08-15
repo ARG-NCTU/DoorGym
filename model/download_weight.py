225289d7488e932d74d9f14cd207635bdaa521a0
kuolun <kuolunwang.ee08@nycu.edu.tw>
2022-08-15 16:00:42 +0800

    update id

--- model/download_weight.py
+++ model/download_weight.py
@@ -4,7 +4,7 @@ import os
 
 # Download all model
 
-dataset_url = 'https://drive.google.com/uc?id=' + "19wVzT3IQazk6NTLTc4tKlw6GEQANMCb6"
+dataset_url = 'https://drive.google.com/uc?id=' + "13hc7elmhEU-9fe5K_KUALVupT519Y8EZ"
 dataset_name = "./model_weight.zip"
 gdown.download(dataset_url, output = dataset_name, quiet=False)
 zip_file = ZipFile(dataset_name)
